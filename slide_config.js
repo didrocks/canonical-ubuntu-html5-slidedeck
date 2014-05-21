f81c8fdb01812c887a5be56fdc3b93ab6e7b2f43
Didier Roche <didrocks@ubuntu.com>
2014-05-21 19:16:28 +0200

    Initial config change for Canonical/Ubuntu branding

--- slide_config.js
+++ slide_config.js
@@ -3,16 +3,15 @@ var SLIDE_CONFIG = {
   settings: {
     title: 'Title Goes Here<br>Up To Two Lines',
     subtitle: 'Subtitle Goes Here',
-    //eventTitle: 'Google I/O 2013',
+    eventTitle: 'Canonical sprint',
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    favIcon: 'images/ubuntu_cof_orange_bg.png',
     fonts: [
-      'Open Sans:regular,semibold,italic,italicsemibold',
-      'Source Code Pro'
+      'Ubuntu:regular,semibold,italic,italicsemibold'
     ],
     //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
   },
@@ -20,18 +19,25 @@ var SLIDE_CONFIG = {
   // Author information
   presenters: [{
     name: 'Firstname Lastname',
-    company: 'Job Title, Google',
-    gplus: 'http://plus.google.com/1234567890',
+    company: 'Job Title, Canonical',
+    email: 'email@canonical.com'
+    /*gplus: 'http://plus.google.com/1234567890',
     twitter: '@yourhandle',
     www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    github: 'http://github.com/you'*/
   }/*, {
     name: 'Second Name',
-    company: 'Job Title, Google',
+    company: 'Job Title, Canonical',
     gplus: 'http://plus.google.com/1234567890',
     twitter: '@yourhandle',
     www: 'http://www.you.com',
     github: 'http://github.com/you'
-  }*/]
+  }*/],
+
+  company: {
+    name: 'Canonical',
+    community_website: 'www.ubuntu.com',
+    website: 'www.canonical.com'
+  }
 };
 
